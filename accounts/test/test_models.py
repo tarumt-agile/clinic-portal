3010ed4a0a2841732d38ec0d3add312d24aa99a6
ligitpizza <ligitpizzaalt@gmail.com>
2026-07-06 18:14:28 +0800

    add docker, yawm, CI and some neccesery project required tools

---
+++ accounts/test/test_models.py
@@ -0,0 +1,8 @@
+import pytest
+from django.contrib.auth.models import User
+
+@pytest.mark.django_db
+def test_create_user():
+    user = User.objects.create_user(username="pizza", password="123")
+    assert user.username == "pizza"
+    assert user.check_password("123")
