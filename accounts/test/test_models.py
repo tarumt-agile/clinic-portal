6cc9175b555328cd575e4e3069972cc55fb774d1
ligitpizza <ligitpizzaalt@gmail.com>
2026-07-06 18:19:19 +0800

    add docker, yawm, CI and some neccesery project required tools

--- accounts/test/test_models.py
+++ accounts/test/test_models.py
@@ -1,6 +1,7 @@
 import pytest
 from django.contrib.auth.models import User
 
+
 @pytest.mark.django_db
 def test_create_user():
     user = User.objects.create_user(username="pizza", password="123")
